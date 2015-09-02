ccf4641bbca802ad55bfb9395d74165348cb7d92
hy-zhang <zhanghaoyuan00@gmail.com>
2015-09-02 17:54:42 +0800

    Interfaces with inheritance.

--- MixinLombok/src/core/lombok/eclipse/handlers/HandleMixin.java
+++ MixinLombok/src/core/lombok/eclipse/handlers/HandleMixin.java
@@ -1,9 +1,6 @@
 package lombok.eclipse.handlers;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 import org.eclipse.jdt.core.compiler.CharOperation;
 import org.eclipse.jdt.core.dom.Modifier;
@@ -31,6 +28,8 @@ public class HandleMixin extends EclipseAnnotationHandler<Mixin> {
 	ASTNode _ast;
 	Annotation _src;
 	Argument[] argFields;
+	HashMap<String, ArrayList<TypeReference>> fieldType; 
+	HashMap<String, TypeReference> hasDefault;
 	
 	@Override public void handle(AnnotationValues<Mixin> annotation, Annotation ast, EclipseNode annotationNode) {
 		pS = ast.sourceStart;
@@ -41,51 +40,30 @@ public class HandleMixin extends EclipseAnnotationHandler<Mixin> {
 		_ast = annotationNode.get();
 		_src = ast;
 		thisAnno = annotationNode;
-		if ((meDecl.modifiers & ClassFileConstants.AccInterface) == 0) {
+		if (!isInterface(meDecl)) {
 			thisAnno.addError("@Mixin is only supported on an interface.");
 			return;
 		}
 		handleMixin();
 	}
 	private void handleMixin() {
-		// Check if a method is static. Has no type parameters.
-		HashMap<String, TypeReference> fieldType = new HashMap<String, TypeReference>();
-		HashMap<String, Boolean> hasSetter = new HashMap<String, Boolean>();
-		for (EclipseNode x : me.down()) {
-			if (!(x.get() instanceof MethodDeclaration)) continue;
-			MethodDeclaration y = (MethodDeclaration) x.get();
-			String methodName = String.valueOf(y.selector);
-			if (y.isDefaultMethod()) continue;
-			if (!(y.returnType instanceof SingleTypeReference)) continue;
-			if (!isVoidMethod(y)) {
-				if (y.arguments != null && y.arguments.length != 0) continue;
-				fieldType.put(methodName, copyType(y.returnType));
-				hasSetter.put(methodName, false);
-			}
-		}
-		for (EclipseNode x : me.down()) {
-			if (!(x.get() instanceof MethodDeclaration)) continue;
-			MethodDeclaration y = (MethodDeclaration) x.get();
-			String methodName = String.valueOf(y.selector);
-			if (isVoidMethod(y) && y.arguments != null && y.arguments.length == 1)
-				if (sameType(fieldType.get(methodName), y.arguments[0].type))
-					hasSetter.put(methodName, true);
-		}
+		fieldType = new HashMap<String, ArrayList<TypeReference>>();
+		hasDefault = new HashMap<String, TypeReference>();
+		addFieldsToMap(me);
+		Iterator<Map.Entry<String, TypeReference>> it = hasDefault.entrySet().iterator();
+		while (it.hasNext()) { fieldType.remove(it.next().getKey()); }
 		argFields = new Argument[fieldType.size()];
-		Iterator<Map.Entry<String, TypeReference>> it = fieldType.entrySet().iterator();
+		Iterator<Map.Entry<String, ArrayList<TypeReference>>> it2 = fieldType.entrySet().iterator();
 		int index = 0;
-		while (it.hasNext()) {
-			Map.Entry<String, TypeReference> entry = it.next();
+		while (it2.hasNext()) {
+			Map.Entry<String, ArrayList<TypeReference>> entry = it2.next();
 			String name = entry.getKey();
-			TypeReference type = entry.getValue();
-			if (!hasSetter.get(name)) {
-				thisAnno.addError("Setter of field " + name + " undefined.");
-				return;
-			}
+			TypeReference type = entry.getValue().get(entry.getValue().size() - 1);
 			argFields[index++] = new Argument(name.toCharArray(), p, copyType(type), Modifier.NONE);
 		}
 		createOfMethod();
 	}
+	// For debugging only.
 	@SuppressWarnings("unused") private void print(String s) {
 		MethodDeclaration print = new MethodDeclaration(meDecl.compilationResult);
 		print.annotations = null;
@@ -177,7 +155,7 @@ public class HandleMixin extends EclipseAnnotationHandler<Mixin> {
 		anonymous.methods = of_methods.length == 0 ? null : of_methods;
 		return anonymous;
 	}
-	private boolean sameType(TypeReference t1, TypeReference t2) {
+	@SuppressWarnings("unused") private boolean sameType(TypeReference t1, TypeReference t2) {
 		if (!(t1 instanceof SingleTypeReference)) return false;
 		if (!(t2 instanceof SingleTypeReference)) return false;
 		return Arrays.equals(((SingleTypeReference) t1).token, ((SingleTypeReference) t2).token);
@@ -185,5 +163,90 @@ public class HandleMixin extends EclipseAnnotationHandler<Mixin> {
 	private boolean isVoidMethod(MethodDeclaration m) {
 		if (!(m.returnType instanceof SingleTypeReference)) return false;
 		return Arrays.equals(TypeConstants.VOID, ((SingleTypeReference) m.returnType).token);
+	}
+	private boolean isInterface(TypeDeclaration t) {
+		return (t.modifiers & ClassFileConstants.AccInterface) != 0;
+	}
+	// Warning: workaround. Only check interfaces in the same file. Inner interfaces unsupported.
+	private EclipseNode findTypeDecl(char[] name) {
+		for (EclipseNode x : me.up().down()) {
+			if (!(x.get() instanceof TypeDeclaration)) continue;
+			TypeDeclaration y = (TypeDeclaration) x.get();
+			if (!isInterface(y)) continue;
+			if (Arrays.equals(name, y.name)) return x;
+		}
+		return null;
+	}
+	// Example 1: (T1 <: T)
+	// interface A { T x(); }
+	// interface B extends A { T1 x(); }
+	// interface C extends A {}
+	// interface D extends B, C {}
+	// D.of(T1 x).
+	// Example 2: (T1 <: T)
+	// interface A { T x(); }
+	// interface B extends A { default T1 x() {...} }
+	// interface C extends A {}
+	// interface D extends B, C {}
+	// D.of().
+	// Example 3: (T1 <: T, T2 <: T)
+	// interface A { T x(); }
+	// interface B extends A { default T1 x() {...} }
+	// interface C extends A { default T2 x() {...} }
+	// interface D extends B, C {}
+	// ERROR.
+	// Example 4: (T1 <: T2 <: T)
+	// interface A { T x(); }
+	// interface B extends A { T1 x(); }
+	// interface C extends A { T2 x(); }
+	// interface D extends B, C {}
+	// D.of(???).
+	private void addFieldsToMap(EclipseNode node) {
+		if (!(node.get() instanceof TypeDeclaration)) return;
+		TypeDeclaration decl = (TypeDeclaration) node.get();
+		if (!isInterface(decl)) return;
+		if (decl.superInterfaces != null) {
+			for (int i = 0; i < decl.superInterfaces.length; i++) {
+				if (!(decl.superInterfaces[i] instanceof SingleTypeReference)) {
+					thisAnno.addError("SuperInterface unresolved.");
+					return;
+				}
+				char[] tempSuper = ((SingleTypeReference) decl.superInterfaces[i]).token;
+				EclipseNode tempSuperDecl = findTypeDecl(tempSuper);
+				if (tempSuperDecl == null) {
+					thisAnno.addError("SuperInterface not found.");
+					return;
+				}
+				addFieldsToMap(tempSuperDecl);
+			}
+		}
+		for (EclipseNode x : node.down()) {
+			if (!(x.get() instanceof MethodDeclaration)) continue;
+			MethodDeclaration y = (MethodDeclaration) x.get();
+			if (y.isDefaultMethod() || isVoidMethod(y)) continue;
+			if (y.arguments != null && y.arguments.length != 0) continue;
+			String name = String.valueOf(y.selector);
+			TypeReference type = copyType(y.returnType);
+			ArrayList<TypeReference> l = new ArrayList<TypeReference>();
+			if (!fieldType.containsKey(name)) {
+				l.add(type);
+			} else {
+				l = fieldType.get(name);
+				// Overridden with the new returnType <: old returnType.
+				// To be validated.
+				if (!l.contains(type)) l.add(type);
+			}
+			fieldType.put(name, l);
+		}
+		for (EclipseNode x : node.down()) {
+			if (!(x.get() instanceof MethodDeclaration)) continue;
+			MethodDeclaration y = (MethodDeclaration) x.get();
+			if (!y.isDefaultMethod() || isVoidMethod(y)) continue;
+			if (y.arguments != null && y.arguments.length != 0) continue;
+			String name = String.valueOf(y.selector);
+			if (fieldType.containsKey(name)) {
+				hasDefault.put(name, copyType(y.returnType));
+			}
+		}
 	}
 }
---
+++ UseMixinLombok/src/test/TestExpression.java
@@ -0,0 +1,68 @@
+package test;
+
+import lombok.Mixin;
+
+public class TestExpression {
+	public static void main(String[] args) {
+		Lit e1 = Lit.of(3);
+		System.out.println(e1.eval());
+		Lit e2 = Lit.of(4);
+		Add e3 = Add.of(e1, e2);
+		System.out.println(e3.eval());
+		Sub e4 = Sub.of(e1, e2);
+		System.out.println(e4.eval());
+		LitP e5 = LitP.of(3);
+		LitP e6 = LitP.of(4);
+		AddP e7 = AddP.of(e5, e6);
+		System.out.println(e5.print() + " = " + e5.eval());
+		System.out.println(e7.print() + " = " + e7.eval());
+	}
+}
+
+interface Exp {
+    int eval();
+}
+
+@Mixin
+interface Lit extends Exp {
+	int x();
+	default int eval() { return x(); }
+}
+
+@Mixin
+interface Add extends Exp {
+	Exp e1();
+	Exp e2();
+	default int eval() {
+		return e1().eval() + e2().eval();
+	}
+}
+
+@Mixin
+interface Sub extends Exp {
+	Exp e1();
+	Exp e2();
+	default int eval() {
+		return e1().eval() - e2().eval();
+	}
+}
+
+interface ExpP extends Exp {
+	String print();
+}
+
+@Mixin
+interface LitP extends Lit, ExpP {
+	default String print() {
+		return "" + x();
+	}
+}
+
+@Mixin
+interface AddP extends Add, ExpP {
+	default String print() {
+		return "(" + e1().print() + " + " + e2().print() + ")";
+	}
+	ExpP e1();
+	ExpP e2();
+}
