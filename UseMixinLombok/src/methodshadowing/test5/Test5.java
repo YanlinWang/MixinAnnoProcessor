ecb1c25b88154fcac07b6bd905aaeb3945c19935
YanlinWang <huohuohuomumu@gmail.com>
2015-11-10 16:31:29 +0800

    revised method overriding mechanism description.

--- UseMixinLombok/src/methodshadowing/test5/Test5.java
+++ UseMixinLombok/src/methodshadowing/test5/Test5.java
@@ -6,8 +6,8 @@ interface A1 { int m(); }
 interface B1 { default int m() { return 0; }}
 interface B2 extends B1 { int m(); }
 interface B3 extends B2 { default int m() {return 1;}}
-interface M1 extends A1, B3 {} //correct
-interface M2 extends A1, B1 {} //error
+interface M1 extends A1, B3 {} //accepted, but should be rejected
+interface M2 extends A1, B1 {} //rejected
 //END_JAVABUG
 
 public class Test5 {
