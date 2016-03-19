276938dcc67a155d092c24875f88ebc82ac8446a
YanlinWang <huohuohuomumu@gmail.com>
2016-03-19 14:22:08 +0300

    case study CZero

---
+++ CZero/src/c0/cj/CZeroNewMain.java
@@ -0,0 +1,20 @@
+package c0.cj;
+
+import java.io.*;
+
+public interface CZeroNewMain {
+    public static void main(String[] args) {
+        String tFile = args[0];
+        FileReader in;
+        try {
+            in = new FileReader(tFile);
+            PrintWriter out;
+            out = new PrintWriter(new FileWriter(tFile.substring(0,tFile.lastIndexOf('.'))+".j"));
+            Constants.codeLibrary(out);
+            Parser.of(tFile, in, out).parseProgram();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
